092e3388bb62042147bef78cd7503716aa4b8499
ArinaCHura <arinaaleksandrova22@gmail.com>
2023-02-06 13:20:37 +0900

    первый коммит

---
+++ main.py
@@ -0,0 +1,11 @@
+from dino_ds_api import dino_ds_api as new_image
+from GAN_core import GAN_core as who_is
+
+img = new_image('dino_random') # выгрузка случайной фотографии с динозавром
+species = who_is(img) # определение вида динозавра по фотографии
+
+print('Selectel')
+print(species)
+
+print(dino_weight(img)) #
+
